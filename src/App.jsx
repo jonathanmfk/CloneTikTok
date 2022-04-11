fe058e289420616ac37c0c5b45ee52976f151b00
Jonathan Molina Fuentes <jonathanmfk.23@gmail.com>
2022-04-11 17:16:23 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+      TikTok clone
+      </main>
+    </div>
+  )
+}
+
+export default App
